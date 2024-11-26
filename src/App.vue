e305a3154b0fd0b8bdc870188eef004ef5f889d2
Alexei <leha52@gmail.com>
2024-11-26 14:26:34 +0500

    feature: add

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,40 @@
 <script setup>
+import { ref, computed } from "vue";
 
+const employees = ref([]);
+
+const name = ref('');
+const date = ref('');
+const daysWorked = ref(1);
+const salary = ref(0);
+
+const addEmployee = () => {
+  if (name.value && date.value && daysWorked.value > 0 && salary.value > 0) {
+    employees.value.push({
+      id: Date.now(),
+      name: name.value,
+      date: date.value,
+      daysWorked: daysWorked.value,
+      salary: salary.value,
+    });
+    name.value = '';
+    date.value = '';
+    daysWorked.value = 1;
+    salary.value = 0;
+  }
+};
+
+
+const removeEmployee = (id) => {
+  employees.value = employees.value.filter((employee) => employee.id !== id);
+};
+
+const totalWithoutTaxes = computed(() => {
+  return employees.value.reduce((sum, emp) => sum + Number(emp.salary), 0);
+});
+const totalWithTaxes = computed(() => {
+  return totalWithoutTaxes.value * 0.85;
+});
 </script>
 
 <template>
